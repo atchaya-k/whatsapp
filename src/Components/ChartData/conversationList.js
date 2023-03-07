0c9f8040033460d56e18c6edc927f9530a8b3f8d
atchaya-k <78771477+atchaya-k@users.noreply.github.com>
2023-03-07 11:32:46 +0530

    Add files via upload

---
+++ src/Components/ChartData/conversationList.js
@@ -0,0 +1,54 @@
+const conversationList = [
+  {
+    'id': 1,
+    'name': 'atchaya',
+    'status':'online',
+    'conversationData': [
+      {
+        'type': 'sent',
+        'message': 'It is a long established fact',
+        'sentAt': '3:08:35 PM',
+      }, {
+        'type': 'received',
+        'message': 'I must explain to you how all this mistaken idea of denouncing ',
+        'sentAt': '3:10:28 PM',
+      }, 
+    ]
+  },
+  {
+    'id': 2,
+    'name': 'Nikitha',
+    'status':'online',
+    'conversationData': [
+      {
+        'type': 'sent',
+        'message': 'English versions from the 1914 translation by H. Rackham',
+        'sentAt': '3:03:28 PM',
+      },
+      {
+        'type': 'received',
+        'message': 'English versions from the 1914 translation by H. Rackham',
+        'sentAt': '3:05:47 PM',
+      },
+      {
+        'type': 'sent',
+        'message': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
+        'sentAt': '3:07:52 PM',
+      }, 
+    ]
+  },
+  {
+    'id': 3,
+    'name': 'Pradeep',
+    'status':'Last seend 2 days ago',
+    'conversationData': [
+      {
+        'type': 'received',
+        'message': 'It is a long established fact',
+        'sentAt': '3:08:35 PM',
+      }
+    ]
+  },
+];
+
+export default conversationList;
