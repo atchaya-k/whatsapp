d3046ad657ec98740bac774e42cace2fd6ae40b6
atchaya-k <78771477+atchaya-k@users.noreply.github.com>
2023-03-07 11:32:17 +0530

    Create ChatsList.js

---
+++ src/Components/ChartData/ChatsList.js
@@ -0,0 +1,28 @@
+
+const ChatsList=[
+    {
+      id:1,
+      name:"Atchaya",
+      message:"I must explain to you how all this mistaken idea of denouncing ",
+      date:"3:10:28 PM",
+      status:'online',
+      type:"receives"
+    },
+    {
+      id:2,
+      name:"Nikitha",
+      message:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      date:"3:07:52 PM",
+      status:'Last seen 2 min ago',
+      type:"send"
+    },
+    {
+      id:3,
+      name:"Pradeep",
+      message:"It is a long established fact",
+      date:"3:08:35 PM",
+      status:'Last seen 2 days ago',
+      type:"read"
+    },
+  ]
+  export default ChatsList;
